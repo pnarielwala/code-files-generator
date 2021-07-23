d3978c235ad80903cec613c1985e4aa4833c91e1
Parth Narielwala <pnarielwala@gmail.com>
2021-07-23 02:05:46 -0400

    add some tests

---
+++ src/index.spec.ts
@@ -0,0 +1,47 @@
+import { CLI } from './index';
+
+import fs from 'fs';
+import chalk from 'chalk';
+
+beforeEach(() => {
+  process.exit = jest.fn<never, never>();
+  console.warn = jest.fn();
+
+  jest.clearAllMocks();
+});
+
+test('check for config file', () => {
+  jest.spyOn(fs, 'existsSync').mockReturnValueOnce(false);
+
+  CLI();
+
+  const configPath = '/template-files-generator.config.js';
+  expect(fs.existsSync).toHaveBeenCalledWith(
+    String(process.env.HOME) + configPath,
+  );
+
+  expect(console.warn).toHaveBeenCalledWith(
+    chalk.yellow(
+      '\nWarning: Pass in config file path or create a config file at ' +
+        `~${configPath}`,
+    ),
+  );
+});
+
+test('check for input config file', () => {
+  jest.spyOn(fs, 'existsSync').mockReturnValueOnce(false);
+
+  const configPath = './config.js';
+  const origArgv = process.argv;
+  process.argv.push(...['-c', configPath]);
+  CLI();
+
+  expect(fs.existsSync).toHaveBeenCalledWith('./config.js');
+  expect(console.warn).toHaveBeenCalledWith(
+    chalk.yellow(
+      '\nWarning: Pass in config file path or create a config file at ' +
+        configPath,
+    ),
+  );
+  process.argv = origArgv;
+});
--- src/index.ts
+++ src/index.ts
@@ -6,90 +6,96 @@ import fs from 'fs';
 import _ from 'lodash';
 import { createCommand } from 'commander';
 
-const program = createCommand();
+export const CLI = () => {
+  const program = createCommand();
 
-program
-  .name('yarn generate')
-  .usage('[options]')
-  .requiredOption(
-    '-c, --config <path-to-config>',
-    'path to config file',
-    `~/template-files-generator.config.js`,
-  )
-  .parse(process.argv);
+  program
+    .name('yarn generate')
+    .usage('[options]')
+    .requiredOption(
+      '-c, --config <path-to-config>',
+      'path to config file',
+      `~/template-files-generator.config.js`,
+    )
+    .parse(process.argv);
 
-const options = program.opts();
+  const options = program.opts();
 
-(async () => {
-  const configPath = options.config.replace(/^~/, String(process.env.HOME));
+  (async () => {
+    const configPath = options.config.replace(/^~/, String(process.env.HOME));
 
-  if (!fs.existsSync(configPath)) {
-    console.warn(
-      chalk.yellow(
-        '\nWarning: Pass in config file path or create a config file at ' +
-          options.config,
-      ),
-    );
-    process.exit(0);
-  }
-
-  const config = await require(configPath);
-
-  const { templateKey } = await inquirer.prompt({
-    type: 'list',
-    name: 'templateKey',
-    message: 'What do you want to generate?',
-    choices: Object.entries(config.templates).map(([key, template]) => ({
-      name: (template as { name: string }).name,
-      value: key,
-    })),
-  });
-
-  const { location } = await inquirer.prompt({
-    name: 'location',
-    message: 'Where do you want to generate it? (e.g. src/components)',
-    filter: (input) => _.trim(input, '/'), // trim leading/trailing whitespace and slashes
-  });
-
-  const variablesConfig: Array<
-    [
-      string,
-      {
-        displayName: string;
-        sideCarAnswers?: (input: string, answers: any) => void;
-      },
-    ]
-  > = Object.entries(config.templates[templateKey].variables);
-  const variables = await inquirer.prompt(
-    variablesConfig.map(([variable, { displayName, sideCarAnswers }]) => ({
-      type: 'input',
-      name: variable,
-      message: displayName,
-      filter: (input, answers) => {
-        sideCarAnswers?.(input, answers);
-        return input;
-      },
-    })),
-  );
-
-  const templateDirectory = path.join(
-    path.dirname(configPath),
-    config.templates[templateKey].directory,
-  );
-  const destinationDirectory = path.join(process.cwd(), location);
-
-  copy(
-    templateDirectory,
-    destinationDirectory,
-    variables,
-    (err, createdFiles) => {
-      if (err) {
-        throw err;
-      }
-
-      createdFiles.forEach((filePath) =>
-        console.log(chalk.green('Created ') + filePath),
+    if (!fs.existsSync(configPath)) {
+      console.warn(
+        chalk.yellow(
+          '\nWarning: Pass in config file path or create a config file at ' +
+            options.config,
+        ),
       );
-    },
-  );
-})();
+      process.exit(0);
+    }
+
+    return;
+
+    const config = await require(configPath);
+
+    const { templateKey } = await inquirer.prompt({
+      type: 'list',
+      name: 'templateKey',
+      message: 'What do you want to generate?',
+      choices: Object.entries(config.templates).map(([key, template]) => ({
+        name: (template as { name: string }).name,
+        value: key,
+      })),
+    });
+
+    const { location } = await inquirer.prompt({
+      name: 'location',
+      message: 'Where do you want to generate it? (e.g. src/components)',
+      filter: (input) => _.trim(input, '/'), // trim leading/trailing whitespace and slashes
+    });
+
+    const variablesConfig: Array<
+      [
+        string,
+        {
+          displayName: string;
+          sideCarAnswers?: (input: string, answers: any) => void;
+        },
+      ]
+    > = Object.entries(config.templates[templateKey].variables);
+    const variables = await inquirer.prompt(
+      variablesConfig.map(([variable, { displayName, sideCarAnswers }]) => ({
+        type: 'input',
+        name: variable,
+        message: displayName,
+        filter: (input, answers) => {
+          sideCarAnswers?.(input, answers);
+          return input;
+        },
+      })),
+    );
+
+    const templateDirectory = path.join(
+      path.dirname(configPath),
+      config.templates[templateKey].directory,
+    );
+    const destinationDirectory = path.join(process.cwd(), location);
+
+    copy(
+      templateDirectory,
+      destinationDirectory,
+      variables,
+      (err, createdFiles) => {
+        if (err) {
+          throw err;
+        }
+
+        createdFiles.forEach((filePath) =>
+          console.log(chalk.green('Created ') + filePath),
+        );
+      },
+    );
+  })();
+};
+
+CLI();
